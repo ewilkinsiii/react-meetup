adcc48050bb646538e6d31a72da4efebc18ee773
railshive <ewilkins@railshive.com>
2023-04-19 14:12:29 -0500

    updated for and get data

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import React, { useRef } from "react";
 import Card from "../ui/Card";
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupFrom = () => {
+const NewMeetupFrom = (props) => {
 
     const titleInputRef = useRef();
     const imageInputRef = useRef();
@@ -27,7 +27,7 @@ const NewMeetupFrom = () => {
             description: enteredDescription
         }
 
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     };
 
     return (
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { userHistory } from 'react-router-dom';
 import NewMeetupFrom from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const NewMeetupPage = () => {
+    const history = useHistory();
+
+    const addMeetupHandler = (meetupData) => {
+        fetch('ENTER URL HERE.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            history.replace('/');
+        });
+    };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupFrom />
+      <NewMeetupFrom onAddMeetup={addMeetupHandler}/>
     </section>
   );
 };
