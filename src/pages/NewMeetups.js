290b95f3b78b430cf7644d942474822029f4d559
railshive <ewilkins@railshive.com>
2023-04-19 14:10:39 -0500

    added form

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,13 @@
-
-import React from 'react';
+import React from "react";
+import NewMeetupFrom from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-
-    return <div> New Meetups Page</div>;
-
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupFrom />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
