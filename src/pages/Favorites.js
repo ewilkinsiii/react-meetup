94bbc312ee641f74f9ae4b8188064193d499a809
railshive <ewilkins@railshive.com>
2023-04-19 17:05:25 -0500

    added badge for favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,24 @@
+import React, { useContext } from "react";
 
-import React from 'react';
-
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 const FavoritesPages = () => {
+  const favoritesCtx = useContext(FavoritesContext);
 
-    return <div>Favorites Page</div>;
+  let content;
 
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p> You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>Favorites Page</h1>
+      {content}
+    </section>
+  );
 };
 
 export default FavoritesPages;
